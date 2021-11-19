338d13ddafab0a2c5df0849d902930fbc1ccbc55
Tamar Alshech <tamaralshech@gmail.com>
2021-11-19 15:15:50 +0200

    14, 2

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --mem=400M
+#SBATCH --mem=1G
 #SBATCH -c4
-#SBATCH --time=1:0:0
+#SBATCH --time=2:0:0
 
-python metropolis_markov_chain.py
+python metropolis_markov_chain.py 14 2
