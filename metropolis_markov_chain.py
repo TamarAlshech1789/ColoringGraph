9c6d7e4269c94f4a14cd1a03dfc41758cc9114b9
Tamar Alshech <tamaralshech@gmail.com>
2021-11-19 15:11:47 +0200

    updates

--- metropolis_markov_chain.py
+++ metropolis_markov_chain.py
@@ -1,17 +1,21 @@
 import sys
 import numpy as np
 import random
-from colorama import Fore
 import timeit
 
 params = {
     'lambda' : 10**3,
+    'lambda_power' : 3,
     'N': 50,
     'marked_cells' : 0,
     'max_marked_cells' : 0,
     'num_no_options' : 0,
     'no_options_cells' : [],
-    'num_iteretions' : 0
+    'num_iteretions' : 0,
+    'num_changing_num_to_0' : 0,
+    'num_changing_0_to_0' : 0,
+    'num_changing_0_to_num' : 0,
+    'num_changing_num_to_num' : 0
 }
 
 global board
@@ -76,32 +80,32 @@ def find_possible_symbols(cell):
         params['no_options_cells'] = []
 
     symbols.append(0)
-    symbols.append(board[row][col])
+    if board[row][col] > 0:
+        symbols.append(board[row][col])
     return symbols
 
 def get_probability(symbols, symbol, board_cell):
-    sum_lambda = 0
-    curr_lambda = 0
+    sum_lambda = 0.0
+    curr_lambda = 0.0
     exponent = params['marked_cells']
-    min_exponent = params['marked_cells']
+    min_exponent = params['lambda_power'] * (exponent)
     if board_cell > 0:
-        min_exponent -= 1
-    exponent -= min_exponent
+        min_exponent = params['lambda_power'] * (exponent - 1)
 
     for s in symbols:
-        add = 0
+        add = 0.0
         if s == 0:
             if board_cell == 0:
-                add = params['lambda'] ** (exponent)
+                add = 10 ** (params['lambda_power'] * (exponent) - min_exponent)
             else:
-                add = params['lambda'] ** (exponent - 1)
+                add = 10 ** (params['lambda_power'] * (exponent - 1) - min_exponent)
             if s == symbol:
                 curr_lambda = add
         else:
             if board_cell == 0:
-                add = params['lambda'] ** (exponent + 1)
+                add = 10 ** (params['lambda_power'] * (exponent + 1) - min_exponent)
             else:
-                add = params['lambda'] ** (exponent)
+                add = 10 ** (params['lambda_power'] * (exponent) - min_exponent)
             if s == symbol:
                 curr_lambda = add
         sum_lambda += add
@@ -109,7 +113,7 @@ def get_probability(symbols, symbol, board_cell):
     return curr_lambda / sum_lambda
 
 def metropolis_RLS():
-    while(params['num_no_options'] < 0.85 * params['N'] ** 2 ):
+    while(params['num_no_options'] < params['N'] ** 2 ):
         cell = random.choice(indices)
         possible_symbols = find_possible_symbols(cell)
         symbol = random.choice(possible_symbols)
@@ -117,44 +121,63 @@ def metropolis_RLS():
         update = np.random.choice([0,1], 1, p=[1 - p, p])
 
         if update == 1:
-            if board[cell[0]][cell[1]] == 0 and symbol > 0:
-                params['marked_cells'] += 1
-            if board[cell[0]][cell[1]] > 0 and symbol == 0:
-                params['marked_cells'] -= 1
+            if board[cell[0]][cell[1]] == 0:
+                if symbol > 0:
+                    params['marked_cells'] += 1
+                    params['num_changing_0_to_num'] += 1
+                else:
+                    params['num_changing_0_to_0'] += 1
+            else:
+                if symbol == 0:
+                    params['marked_cells'] -= 1
+                    params['num_changing_num_to_0'] += 1
+                else:
+                    params['num_changing_num_to_num'] += 1
+
             board[cell[0]][cell[1]] = symbol
         params['max_marked_cells'] = max(params['max_marked_cells'], params['marked_cells'])
         params['num_iteretions'] += 1
 
-for N in range(20, 100, 20):
-    for e in range(2,10):
-        #initial params
-        params['N'] = N
-        params['num_iteretions'] = 0
-        params['marked_cells'] = params['N']
-        params['max_marked_cells'] = 0
-        params['num_no_options'] = 0
-        params['no_options_cells'] = []
-        params['num_iteretions'] = 0
+N = int(sys.argv[1])
+e = int(sys.argv[2])
 
-        board = np.zeros(params['N'] ** 2).reshape((params['N'], params['N']))
-        for i in range(params['N']):
-            board[i][i] = i + 1
-            # board[i][params['N'] - i - 1] = (i + 2) % params['N'] + 1
-        indices = list(np.ndindex(board.shape))
+#for N in range(20, 100, 20):
+    #for e in range(2,10):
+#initial params
+params['N'] = N
+params['lambda_power'] = e
+params['num_iteretions'] = 0
+params['marked_cells'] = params['N']
+params['max_marked_cells'] = 0
+params['num_no_options'] = 0
+params['no_options_cells'] = []
+params['num_iteretions'] = 0
+params['num_changing_0'] = 0
+
+board = np.zeros(params['N'] ** 2).reshape((params['N'], params['N']))
+for i in range(params['N']):
+    board[i][i] = i + 1
+    # board[i][params['N'] - i - 1] = (i + 2) % params['N'] + 1
+indices = list(np.ndindex(board.shape))
 
 
-        params['lambda'] = 10**e
-        file_name = 'metropolis_borad_N_' +  str(params['N']) + '_lambda_10e' + str(e) + '.txt'
-        sys.stdout = open(file_name, "w")
-        start = timeit.default_timer()
-        metropolis_RLS()
-        print('*****************************************************')
-        print(file_name)
-        print('*****************************************************')
-        print('number of placements -', params['marked_cells'])
-        print('cover ', params['marked_cells'] / N**2, ' per. of the cells')
-        print_solution()
-        end = timeit.default_timer()
-        print('running time- ', (end - start), ' sec.')
+params['lambda'] = 10**e
+file_name = 'metropolis_borad_N_' +  str(params['N']) + '_lambda_10e' + str(e) + '.txt'
+#sys.stdout = open(file_name, "w")
+start = timeit.default_timer()
+metropolis_RLS()
+print('*****************************************************')
+print(file_name)
+print('*****************************************************')
+print('number of placements -', params['marked_cells'])
+print('cover ', params['marked_cells'] / N**2, ' per. of the cells')
+print_solution()
+end = timeit.default_timer()
+print('running time- ', (end - start), ' sec.')
+print('*****************************************************')
+print('number of changes 0 to 0-', params['num_changing_0_to_0'], ' times')
+print('number of changes 0 to num-', params['num_changing_0_to_num'], ' times')
+print('number of changes num to 0-', params['num_changing_num_to_0'], ' times')
+print('number of changes num to num-', params['num_changing_num_to_num'], ' times')
 
-        sys.stdout.close()
+#sys.stdout.close()
